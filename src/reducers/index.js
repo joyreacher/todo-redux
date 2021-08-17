b5f2128a7a7f0090a3c2f2498c5fb0b5dfdacfe7
Brian Thomas <btdeveloper125@gmail.com>
2021-08-17 11:36:26 -0600

    feat: This commit adds the delete reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {
   ADD_TODO,
-  TOGGLE_TODO
+  TOGGLE_TODO,
+  DELETE_TODO
 } from '../actions'
 
 function todos (state = [], action) {
@@ -14,8 +15,10 @@ function todos (state = [], action) {
         }
       ]
     case TOGGLE_TODO:
-      return state.map((todo, index) => (index === action.index) ? { ...todo, completed: !todo.completed } : todo
-      )
+      return state.map((todo, index) => (index === action.index) ? { ...todo, completed: !todo.completed } : todo)
+    case DELETE_TODO:
+      state.splice(action.index, 1)
+      return state
   }
 }
 export default todos
