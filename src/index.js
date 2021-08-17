d3f9527b86fb6fe06eef4ad7e0a4a61edf2f4335
Brian Thomas <btdeveloper125@gmail.com>
2021-08-17 09:44:51 -0600

    style: This commit removes unsed code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import todos from './reducers'
 import { addTodo, toggleTodo } from './actions';
-ReactDOM.render(<App />, document.getElementById('root'));
 const store = createStore(todos);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
 console.log(store)
 store.dispatch(addTodo('Dispatch my first action'));
 console.log('Initial State ', store.getState());
