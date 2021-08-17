39934d9bf004e6087b8a96e8c9452db457d70d2d
Brian Thomas <btdeveloper125@gmail.com>
2021-08-17 11:37:26 -0600

    refactor: This commit adds practice with building and disptaching actions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import { createStore } from 'redux'
 import todos from './reducers'
-import { addTodo, toggleTodo } from './actions';
+import { addTodo, toggleTodo, deleteTodo } from './actions';
 const store = createStore(todos);
 console.log(store)
-store.dispatch(addTodo('Dispatch my first action'));
-console.log('Initial State ', store.getState());
 
-store.dispatch(addTodo('Dispatch my second action'))
-console.log('State after adding first todo', store.getState())
+// Add to do item
+store.dispatch(addTodo('Feed goldfish', '😢 😢'));
+console.log('%cInitial State with 1st to do item', 'color: yellow; font-family: Courier; font-size:20px', store.getState());
 
-store.dispatch(toggleTodo(0))
-console.log('State after toggling first todo', store.getState())
+store.dispatch(addTodo('Walk cat'))
+console.log('%cState after adding 2nd todo', 'color: yellow; font-family: Courier; font-size:20px', store.getState())
+
+store.dispatch(addTodo('Wash feet'))
+console.log('%cState after adding 3rd todo', 'color: yellow; font-family: Courier; font-size:20px', store.getState())
+
+store.dispatch(addTodo('Listen to plants'))
+console.log('%cState after adding 4th todo', 'color: yellow; font-family: Courier; font-size:20px', store.getState())
+
+store.dispatch(addTodo('Sleep'))
+console.log('%cState after adding 4th todo', 'color: yellow; font-family: Courier; font-size:20px', store.getState())
+
+// Toggle
+store.dispatch(toggleTodo(1))
+console.log('%cState after toggling first todo', 'color: pink; font-family: Courier; font-size:20px', store.getState())
+
+// Delete
+store.dispatch(deleteTodo(1))
+console.log('%cState after deleting second todo', 'color: red; font-family: Courier; font-size:16px', store.getState())
+
+// Show all state data
+console.table(store.getState())
