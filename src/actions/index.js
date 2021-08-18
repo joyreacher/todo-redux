7cc30bed37617f8f4067b5c14576daf7ed6f7287
Brian Thomas <btdeveloper125@gmail.com>
2021-08-18 12:33:40 -0600

    feat: This commit adds register action

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const DELETE_TODO = 'DELETE_TODO'
 export const LOGIN = 'LOGIN'
+export const REGISTER = 'REGISTER'
 //Action createors
 export const addTodo = text => ({
   type: ADD_TODO,
@@ -23,4 +24,12 @@ export const login = (username, password) => ({
   type: LOGIN,
   username,
   password
+})
+
+export const register = (username, password, email, birthday) => ({
+  type: REGISTER,
+  username,
+  password,
+  email,
+  birthday
 })
